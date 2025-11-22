886f501bdab1016c84a5b6de66f99474c512e6ad
MaxQuint <maximoquinteroescobar8@gmail.com>
2025-11-21 19:48:45 -0600

    add menu and tower

--- cw-game/Assets/Scripts/UI/MainMenu.cs
+++ cw-game/Assets/Scripts/UI/MainMenu.cs
@@ -33,7 +33,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         // SceneManager.LoadScene("Level1");
-        SceneManager.LoadScene("Scene Max");
-        // SceneManager.LoadScene("Tower");
+        // SceneManager.LoadScene("Scene Max");
+        SceneManager.LoadScene("Tower");
     }
 }
